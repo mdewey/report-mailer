e6843e8e79d0f7d2e6603414130ad4875e1215ea
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-16 12:40:44 -0500

    refactored to use data folder

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,6 @@ const client = new postmark.ServerClient("d94d87f4-9883-4d13-bcbd-7b7fab6ea0f9")
 import format = require('date-fns/format')
 import fs = require("fs");
 
-import Student from './student';
-import data from '../data.students.json';
-
-
-
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
     // read binary data
@@ -19,28 +14,27 @@ function base64_encode(file) {
     return new Buffer(bitmap).toString('base64');
 }
 
-// Send an email:
+import Student from './student';
 
-const students: Array<Student> = [
-    new Student("Billy Bob", "mtdewey55@gmail.com")
-]
+const _data:Array<{name:string, email:string}> =  require("../data/students.json"); 
+const students:Array<Student> = _data.map(m => new Student(m.name, m.email));
+
 
 var today = format(
     new Date(),
     'MM/DD/YYYY'
 );
 
-
 students.forEach(student => {
     const _fileName: string = `${student.firstName}.pdf`;
-    const _reportData: string = base64_encode(`${__dirname}\\reports\\${_fileName}`);
-    client.sendEmail({
-        "From": "mark@suncoast.io",
-        "To": "mark@suncoast.io",
-        "Subject": `Progress Report for ${today}`,
-        "HtmlBody": `Hello, ${student.firstName} <br/> Attached is your progress report!`,
-        "Attachments": [
-            new postmark.Attachment(_fileName, _reportData, "application/pdf")
-        ]
-    });
+    const _reportData: string = base64_encode(`${__dirname.replace("build", "data")}\\reports\\${_fileName}`);
+    // client.sendEmail({
+    //     "From": "mark@suncoast.io",
+    //     "To": "mark@suncoast.io",
+    //     "Subject": `Progress Report for ${today}`,
+    //     "HtmlBody": `Hello, ${student.firstName} <br/> Attached is your progress report!`,
+    //     "Attachments": [
+    //         new postmark.Attachment(_fileName, _reportData, "application/pdf")
+    //     ]
+    // });
 })
